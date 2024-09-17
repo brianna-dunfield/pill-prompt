52f9197361edcd40c5ba115736650cd2eefcdfd4
Brianna Dunfield <bdunfield@mta.ca>
2024-09-17 19:41:45 -0300

    finished nav bar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,21 @@ import logo from '../../assets/logo/pill_prompt_logo.png';
 export default function NavBar() {
 	return (
 		<nav className='nav'>
-			<img
-				className='nav__logo'
-				src={logo}
-				alt='Pill with blue top and green bottom rotated 45 degrees to the right with the words pill prompt underneath in blue.'
-			/>
-			<Link className='nav__button' to='/'>Login</Link>
+			<Link className='nav__logo'>
+				<img
+					className='nav__logo-img'
+					src={logo}
+					alt='Pill with blue top and green bottom rotated 45 degrees to the right.'
+				/>
+				<h1 className='nav__logo-text'>Pill Prompt</h1>
+			</Link>
+
+			<Link
+				className='nav__button'
+				to='/'
+			>
+				Login
+			</Link>
 		</nav>
 	);
 }
