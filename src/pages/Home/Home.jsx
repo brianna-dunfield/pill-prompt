821cf72c2d917c4e44e98cbc97f5e8af116c4aa9
Brianna Dunfield <bdunfield@mta.ca>
2024-09-17 22:59:52 -0300

    Beginning of medication display

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import Header from '../../components/Header/Header.jsx';
 
 export default function Home(){
     return <main className='home'>
-    <Header headerText={'Home'} returnUrl={'/'} editUrl={'/'}/>
     <p>Hello</p>
     </main>
 }
--- src/pages/MedicationDisplay/MedicationDisplay.jsx
+++ src/pages/MedicationDisplay/MedicationDisplay.jsx
@@ -1,5 +1,17 @@
 import './MedicationDisplay.scss';
+import Header from '../../components/Header/Header.jsx';
+import { useParams } from 'react-router-dom';
 
 export default function MedicationDisplay() {
-	return <></>;
+	let { userId } = useParams();
+
+	return (
+		<main className='medication-display'>
+			<Header
+				headerText='Medications'
+				returnUrl='/'
+				editUrl={`/add-medication/${userId}`}
+			/>
+		</main>
+	);
 }
