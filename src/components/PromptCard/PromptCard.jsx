3c753e6596d20d09b68e18b9968e3291aeb43b2c
Brianna Dunfield <bdunfield@mta.ca>
2024-09-22 23:50:49 -0300

    tablet and desktop prompt

--- src/components/PromptCard/PromptCard.jsx
+++ src/components/PromptCard/PromptCard.jsx
@@ -45,81 +45,83 @@ export default function PromptCard({
 	};
 
 	return (
-		<article className='prompt'>
-			<h2 className='prompt__header'>Take Medication</h2>
-			<img
-				className='prompt__image'
-				src={`http://localhost:8080/images/${medicationName}.jpg`}
-				alt={`${medicationName} pill image`}
-			/>
-			<div className='prompt__main-info'>
-				<div>
-					<p className='prompt__label'>Medication Name</p>
-					<p className='prompt__info'>
-						{medicationInformation.medication_name}
-					</p>
-				</div>
-				<div>
-					<p className='prompt__label'>Time of Dose</p>
-					<p className='prompt__info'>
-						{medicationInformation.medication_dose_time}
-					</p>
-				</div>
-			</div>
-			<div className='prompt__dose-info'>
-				<div>
-					<p className='prompt__label'>Number of Tablets</p>
-					<p className='prompt__info'>
-						{medicationInformation.user_dosage /
-							medicationInformation.pill_dosage}
-					</p>
-				</div>
-				<div>
-					<p className='prompt__label'>Total Dose</p>
-					<p className='prompt__info'>
-						{`${medicationInformation.user_dosage} mg`}
-					</p>
-				</div>
-			</div>
-			<div className='prompt__option'>
+		<div className='page-body'>
+			<article className='prompt'>
+				<h2 className='prompt__header'>Take Medication</h2>
 				<img
-					className='prompt__option-yes'
-					src={check}
-					alt='Green checkmark in a green circle'
+					className='prompt__image'
+					src={`http://localhost:8080/images/${medicationName}.jpg`}
+					alt={`${medicationName} pill image`}
 				/>
-				<img
-					className='prompt__option-later'
-					src={reminder}
-					alt='Yellow clock'
-				/>
-			</div>
-			<div className='prompt__navigation'>
-				{first ? (
-					<button
-						onClick={handleNextClick}
-						className='hide'
-					>
-						PREVIOUS
-					</button>
-				) : (
-					<button onClick={handleNextClick}>PREVIOUS</button>
-				)}
-				{last ? (
-					<button
-						className='hide'
-						onClick={handlePreviousClick}
-					>
-						NEXT
-					</button>
-				) : (
-					<button
-						className='prompt__navigation-next {}'
-						onClick={handlePreviousClick}
-					>
-						NEXT
-					</button>
-				)}
-			</div>
-		</article>
+				<div className='prompt__main-info'>
+					<div>
+						<p className='prompt__label'>Medication Name</p>
+						<p className='prompt__info'>
+							{medicationInformation.medication_name}
+						</p>
+					</div>
+					<div>
+						<p className='prompt__label'>Time of Dose</p>
+						<p className='prompt__info'>
+							{medicationInformation.medication_dose_time}
+						</p>
+					</div>
+				</div>
+				<div className='prompt__dose-info'>
+					<div>
+						<p className='prompt__label'>Number of Tablets</p>
+						<p className='prompt__info'>
+							{medicationInformation.user_dosage /
+								medicationInformation.pill_dosage}
+						</p>
+					</div>
+					<div>
+						<p className='prompt__label'>Total Dose</p>
+						<p className='prompt__info'>
+							{`${medicationInformation.user_dosage} mg`}
+						</p>
+					</div>
+				</div>
+				<div className='prompt__option'>
+					<img
+						className='prompt__option-yes'
+						src={check}
+						alt='Green checkmark in a green circle'
+					/>
+					<img
+						className='prompt__option-later'
+						src={reminder}
+						alt='Yellow clock'
+					/>
+				</div>
+				<div className='prompt__navigation'>
+					{first ? (
+						<button
+							onClick={handleNextClick}
+							className='hide'
+						>
+							PREVIOUS
+						</button>
+					) : (
+						<button onClick={handleNextClick}>PREVIOUS</button>
+					)}
+					{last ? (
+						<button
+							className='hide'
+							onClick={handlePreviousClick}
+						>
+							NEXT
+						</button>
+					) : (
+						<button
+							className='prompt__navigation-next {}'
+							onClick={handlePreviousClick}
+						>
+							NEXT
+						</button>
+					)}
+				</div>
+			</article>
+		</div>
 	);
 }
